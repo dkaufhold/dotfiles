f5198e1e8ed50536f1ac8f07283615352fff0599
Dan Kaufhold <kaufhold.d@gmail.com>
2022-05-28 06:58:43 +0000

    Installed oh-my-zsh and powerlevel10k

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/env sh
+
+
+# Oh My ZSH
+sudo apt-get --yes install zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+cat zshrc_echo_1 > $HOME/.zshrc
+
+
